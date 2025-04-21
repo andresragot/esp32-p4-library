b365a1f5512a7595bd25374523901eeb4ea30ff2
Andrés Ragot <andresragot99@gmail.com>
2025-04-21 20:10:15 +0200

    Update main.cpp

--- p4-graphics/main/main.cpp
+++ p4-graphics/main/main.cpp
@@ -2,12 +2,22 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <vector>
+#include "CommonTypes.hpp"
+#include "Scene.hpp"
+#include "RevolutionMesh.hpp"
+#include "ExtrudeMesh.hpp"
+#include "Id.hpp"
+
 
 static const char* MAIN_TAG = "Main";
 
+using namespace std;
+using namespace Ragot;
+
 extern "C" void app_main(void)
 {
-    esp_log_level_set("*", ESP_LOG_NONE);
+    esp_log_level_set("*", ESP_LOG_INFO);
 
     ESP_LOGI(MAIN_TAG, "Iniciando aplicación");
     ESP_LOGI(MAIN_TAG, "Memoria libre inicial: %u bytes", esp_get_free_heap_size());
@@ -18,17 +28,49 @@ extern "C" void app_main(void)
     uint32_t frame_count = 0;
     ESP_LOGI(MAIN_TAG, "Entrando en bucle de renderizado");
 
+    Camera camera(1.f, 0.1f, 1000.f, 60.f);
+    Scene scene (&camera);
+
+    vector < coordinates_t > coords = { { 0 ,  0 }, { 4 ,  0 },
+                                        { 2 ,  2 }, { 2 ,  5 },
+                                        { 4 ,  7 }, { 6 ,  9 },
+                                        { 0 ,  9 } };
+
+    vector < coordinates_t > coords_cube = { { 0 ,  0 } , { 1 ,  1 },
+                                             { 2 ,  1 } , { 3 ,  0 },
+                                             { 2 , -1 } , { 1 , -1 },
+                                             { 0 ,  0 } , { 1 ,  1 },
+                                             { 2 ,  1 } , { 3 ,  0 },
+                                             { 2 , -1 } , { 1 , -1 } };
+
+    mesh_info_t mesh_info_cup  (coords, RENDER_REVOLUTION);
+    mesh_info_t mesh_info_cube (coords_cube, RENDER_EXTRUDE);
+
+    // RevolutionMesh mesh_cup  (mesh_info_cup );
+    ExtrudeMesh    mesh_cube (mesh_info_cube);
+
+    // mesh_cup.set_position  (glm::vec3{ 0.f, 0.f, -5.f });
+    mesh_cube.set_position (glm::vec3{ 0.f, 0.f, -5.f });
+
+
+    // scene.add_node(&mesh_cup , ID(COPA));
+    scene.add_node(&mesh_cube, ID(CUBO));
+
+    renderer.set_scene(&scene);
+
     while (true)
     {
-        ESP_LOGI(MAIN_TAG, "--- Frame %u ---", frame_count);
-        renderer.render_debug();
-        ESP_LOGI(MAIN_TAG, "Esperando 50ms");
-        vTaskDelay(pdMS_TO_TICKS(50));
+        ESP_LOGI(MAIN_TAG, "\n--- Frame %u ---", frame_count);
+        // renderer.render_debug();
+        renderer.render();
+        // ESP_LOGI(MAIN_TAG, "Esperando 50ms");
+        // vTaskDelay(pdMS_TO_TICKS(50));
         
         frame_count++;
         
         // Log periódico del estado de memoria
-        if (frame_count % 100 == 0) {
+        if (frame_count % 100 == 0) 
+        {
             ESP_LOGI(MAIN_TAG, "Estado después de %u frames:", frame_count);
             ESP_LOGI(MAIN_TAG, "Memoria libre: %u bytes", esp_get_free_heap_size());
         }
