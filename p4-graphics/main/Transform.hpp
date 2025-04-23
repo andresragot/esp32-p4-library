232af9116ea8e9150c48d90fa46514398252da9b
Andrés Ragot <andresragot99@gmail.com>
2025-04-23 20:29:45 +0200

    Update Transform.hpp

--- p4-graphics/main/Transform.hpp
+++ p4-graphics/main/Transform.hpp
@@ -22,36 +22,62 @@ namespace Ragot
         vec3 position;
         vec3 rotation;
         vec3 scale;
+        bool dirty = true;
+
+    private:
+        mat4 transform_matrix;
         
     public:
         Transform () : position (0.f), rotation (0.f), scale (1.f) {}
         virtual ~Transform () = default;
         
     public:
-        void set_position (const vec3 & pos) { position = pos; }
+        void set_position (const vec3 & pos) 
+        { 
+            position = pos; 
+            dirty = true; 
+        }
         vec3 get_position () const { return position; }
         
-        void set_rotation (const vec3 & rot) { rotation = rot; }
+        void set_rotation (const vec3 & rot) 
+        {
+            rotation = rot; 
+            dirty = true; 
+        }
         vec3 get_rotation () const { return rotation; }
 
         void rotate (const float angle, const vec3 & axis)
         {
             rotation += angle * axis;
             if (rotation.x > 360.f) rotation.x -= 360.f;
+            dirty = true;
         }
         
-        void set_scale (const vec3 & scale) {this->scale = scale; }
+        void set_scale (const vec3 & scale) 
+        {
+            this->scale = scale; 
+            dirty = true;
+        }
         vec3 get_scale () const { return scale; }
+
+        bool is_dirty () const { return dirty; }
         
     public:
         virtual mat4 get_transform_matrix ()
         {
-            mat4 transform_matrix(1);
-            transform_matrix = glm::translate(transform_matrix, position);
-            transform_matrix = glm::scale (transform_matrix, scale);
-            
-            glm::quat quaternion_rotation = glm::quat (glm::radians (rotation));
-            transform_matrix *= glm::mat4_cast (quaternion_rotation);
+            if (dirty)
+            {
+                dirty = false;
+                
+                mat4 identity(1);
+                identity = glm::translate(identity, position);
+                identity = glm::scale (identity, scale);
+                
+                glm::quat quaternion_rotation = glm::quat (glm::radians (rotation));
+                identity *= glm::mat4_cast (quaternion_rotation);
+
+                transform_matrix = identity;
+            }
             
             return transform_matrix;
         }
